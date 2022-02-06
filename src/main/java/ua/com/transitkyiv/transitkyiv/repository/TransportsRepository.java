3c6eb220cf9112115e6e7b60821d211c41ed992a
dpointtt <dpointttwink@gmail.com>
2022-02-06 06:23:39 +0200

    Deleting unnecessary

--- src/main/java/ua/com/transitkyiv/transitkyiv/repository/TransportsRepository.java
+++ src/main/java/ua/com/transitkyiv/transitkyiv/repository/TransportsRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.transitkyiv.transitkyiv.entity.Transports;
 
 public interface TransportsRepository extends JpaRepository<Transports, Long> {
-    Transports findTransportsByTransportEquals(String Transport);
 }
